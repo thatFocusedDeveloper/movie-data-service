dad34ac893ce4349d0bb5addaed8269401a22b0c
sshekhar3 <Sunny_Shekhar@intuit.com>
2024-10-19 17:51:40 +0530

    First Commit

---
+++ src/main/java/io/tribe/movie_data_service/MovieDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.tribe.movie_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tribe/movie_data_service/MovieDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tribe.movie_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
