1a2981c7707ea4de91680aea18170e7292c7439c
sshekhar3 <Sunny_Shekhar@intuit.com>
2024-10-23 19:43:21 +0530

    08. Calling Ratings Data API

--- src/main/java/io/tribe/movie_data_service/resources/RatingsResources.java
+++ src/main/java/io/tribe/movie_data_service/resources/RatingsResources.java
@@ -1,6 +1,8 @@
 package io.tribe.movie_data_service.resources;
 
 import io.tribe.movie_data_service.models.Rating;
+import io.tribe.movie_data_service.models.UserRating;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/ratingsdata")
 public class RatingsResources {
-    public Rating getRating(@PathVariable("movieId") String movieid ) {
+    
+    @GetMapping("/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieid ) {
         return new Rating("John Wick", 8);
+    }
+    
+    @GetMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
     }
 }
